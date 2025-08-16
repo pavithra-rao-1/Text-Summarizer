acd00ed3a5f434dee90b276cff124989f4c24e39
pavithra-rao-1 <pavithrarao2003@gmail.com>
2025-08-16 17:23:44 +0530

    research

--- summa.py
+++ summa.py
@@ -1,3 +1,3 @@
-from textSummarizer.logging import logger
-
-logger.info("Starting the text summarization pipeline...")
+import accelerate
+print("Accelerate is imported from:", accelerate.__file__)
+print("Accelerate version:", accelerate.__version__)
